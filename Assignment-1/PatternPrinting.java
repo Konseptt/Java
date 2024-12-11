9a12298ce27b2ef0156cb9a6699019e893fe6f85
konseptt <sharmaranjan816@gmail.com>
2024-12-11 15:28:37 +0530

    commit

--- Assignment-1/PatternPrinting.java
+++ Assignment-1/PatternPrinting.java
@@ -1,7 +1,7 @@
-public class Pattern {
+public class PatternPrinting {
     public static void main(String[] args) {
-        // a
-        System.out.println("Pattern a: ");
+        // Pattern a: *****
+        System.out.println("Pattern a:");
         for (int i = 5; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
@@ -9,11 +9,12 @@ public class Pattern {
             System.out.println();
         }
 
-        System.out.println();
-
-        // b
-        System.out.println("Pattern b: ");
+        // Pattern b: *
+        System.out.println("\nPattern b:");
         for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5 - i; j++) {
+                System.out.print(" ");
+            }
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
