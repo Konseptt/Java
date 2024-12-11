27db26b9adfd859815fc47b35ee3c64b6f725f2f
konseptt <sharmaranjan816@gmail.com>
2024-12-11 23:03:20 +0530

    commit

--- Assignment-1/Complexexpression.java
+++ Assignment-1/Complexexpression.java
@@ -4,7 +4,6 @@ public class Complexexpression {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Input values for a, b, c, d, e, f, and g
         System.out.print("Enter value for a: ");
         int a = scanner.nextInt();
         System.out.print("Enter value for b: ");
@@ -19,8 +18,6 @@ public class Complexexpression {
         int f = scanner.nextInt();
         System.out.print("Enter value for g: ");
         int g = scanner.nextInt();
-
-        // Evaluate the expression
         if (d - e == 0) {
             System.out.println("Error: Division by zero in the expression.");
         } else {
